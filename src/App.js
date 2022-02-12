db3c9de7c5857a07a7a7d29310098a15ebfd1355
AhmedPwn <orgsoccer007@gmail.com>
2022-02-12 21:28:48 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState,useEffect} from 'react';
+import PokemonList from './PokemonList'
+import axios from 'axios'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+  const [pokemon,setPokemon] = useState(["a1"])
+  const [prevPage,setPrevPage] = useState()
+  const [nextPage,setNextPage] = useState()
+
+  axios.get("https://pokeapi.co/api/v2/pokemon").then(res=>{
+    setPokemon(res.data.results.map(p => p.name))
+  })
+
+  return ( 
+    <>
+      <PokemonList pokemon={pokemon}/>
+    </>
   );
 }
 
